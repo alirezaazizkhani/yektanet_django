80d719163121f4f47615a914c638aa62c0f24cea
alireza <alireza.azizkhani.2003@gmail.com>
2024-02-25 13:48:11 +0330

    main template added

--- mangement_advertiser/models.py
+++ mangement_advertiser/models.py
@@ -16,10 +16,10 @@ class Ad(models.Model):
     ID = models.IntegerField(primary_key=True)
     Title = models.CharField(max_length=100)
     imgUrl = models.CharField(max_length=255)
-    Link = models.CharField(max_length=255)
+    Link = models.URLField(max_length=255)
     Clicks = models.IntegerField(default=0)
     Views = models.IntegerField(default=0)
-    Advertiser = models.ForeignKey('Advertiser', on_delete=models.CASCADE)
+    advertiser = models.ForeignKey('Advertiser', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.Title
--- mangement_advertiser/views.py
+++ mangement_advertiser/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
+# from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Ad, Advertiser
+
+
+def main(request):
+    advertiseres = Advertiser.objects.all()
+    data = []
+    for advertiser in advertiseres:
+        data.append({
+            'advertiser': advertiser,
+            'ads': Ad.objects.filter(advertiser__ID = advertiser.ID)
+        })
+    return render(request, 'mangement_advertiser/main.html', {"data": data})
