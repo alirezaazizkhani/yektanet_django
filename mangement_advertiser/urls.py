bdb704b0a495ec6eeed1272e6fc3456cd24f5927
alireza <alireza.azizkhani.2003@gmail.com>
2024-02-25 14:14:41 +0330

    ad_handler view added

--- mangement_advertiser/urls.py
+++ mangement_advertiser/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main , name="main"),
-    
+    path('ad/<int:ad_id>/', views.ad_handler, name="ad_handler"),
+    path('add_ads', views.add_ads, name="add_ads"),
 ]
--- mangement_advertiser/views.py
+++ mangement_advertiser/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-# from django.template import loader
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from .models import Ad, Advertiser
 
@@ -14,3 +13,21 @@ def main(request):
             'ads': Ad.objects.filter(advertiser__ID = advertiser.ID)
         })
     return render(request, 'mangement_advertiser/main.html', {"data": data})
+
+def ad_handler(request, ad_id):
+    try : 
+        ad = Ad.objects.get(ID=ad_id)
+    except Ad.DoesNotExist:
+        raise Http404("Ad does not exist")
+    ad.Clicks += 1
+    ad.Views += 1
+    advertiser = ad.advertiser
+    advertiser.Clicks += 1
+    advertiser.Views += 1
+    ad.save()
+    advertiser.save()
+    return redirect(ad.Link)
+
+
+def add_ads(request, ad_id):
+    pass
